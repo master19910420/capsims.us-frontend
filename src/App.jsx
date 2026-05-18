dbc5b8b5b3c9874b73bd61dcbbc1e9e892497e47
max-doppelt <superdev0420@gmail.com>
2026-05-18 00:49:43 -0400

    update'

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import InvitePage from './pages/InvitePage'
 import Completed from './pages/Completed'
 import NotFound from './pages/NotFound'
 
-const wecreateproblems_URL = 'https://wecreateproblems.com/'
+const wecreateproblems_URL = 'https://sigmaassessmentsystems.com/'
 
 function RedirectTowecreateproblems() {
   useEffect(() => {
